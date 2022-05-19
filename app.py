75ab31e2a6ed9d32e4312d83468b01e123b5774e
AdminTima <tima.khilkevich@inbox.ru>
2022-05-19 15:00:12 +0300

    Added ability to add tasks using form on the index page. Also added templates folder

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    complete = db.Column(db.Boolean)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    todo_lst = Todo.query.all()
+    return render_template('base.html', todo_lst=todo_lst)
 
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
+if __name__=='__main__':
+    db.create_all()
+    app.run()
